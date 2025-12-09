bba1e4d722d1aa7849b85a7b59d57200d23c5be6
usaid <sayedusaid49@gmail.com>
2025-12-09 21:05:26 +0530

    File commented

--- frontend/src/Pages/GameStream.tsx
+++ frontend/src/Pages/GameStream.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import HangWatchWrapper from "../components/Stream/HangWatch";
+// import React from "react";
+// import HangWatchWrapper from "../components/Stream/HangWatch";
 
-const GameStream: React.FC = () => {
-  return (
-    <div className="dark:bg-black ">
-      {/* Optional: you can do the same wrapper for <hang-support> if needed */}
-        {/* <hang-support mode="dark" show="true"></hang-support> */}
-        {/* HangWatch component for the game stream */}
-      <HangWatchWrapper />
-    </div>
-  );
-};
+// const GameStream: React.FC = () => {
+//   return (
+//     <div className="dark:bg-black ">
+//       {/* Optional: you can do the same wrapper for <hang-support> if needed */}
+//         {/* <hang-support mode="dark" show="true"></hang-support> */}
+//         {/* HangWatch component for the game stream */}
+//       <HangWatchWrapper />
+//     </div>
+//   );
+// };
 
-export default GameStream;
+// export default GameStream;
