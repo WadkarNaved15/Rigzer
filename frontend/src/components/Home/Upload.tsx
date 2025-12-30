f8319977deea1e4b9559a78fc74de3d2a4941674
usaid <sayedusaid49@gmail.com>
2025-12-30 20:28:15 +0530

    Post button changed

--- frontend/src/components/Home/Upload.tsx
+++ frontend/src/components/Home/Upload.tsx
@@ -6,9 +6,9 @@ export default function UploadBox({ onUploadClick }: { onUploadClick: () => void
       <button
         onClick={onUploadClick}
         className="min-w-full h-10 px-4 flex items-center justify-center 
-        rounded-lg border border-red-300 bg-red-500 
+        rounded-lg border border-[#1e1e1e]-300 bg-[#1e1e1e]-500
         text-white text-sm font-medium tracking-wide 
-        hover:bg-red-600 hover:border-red-400 transition"
+        hover:bg-[#1e1e1e]-600 hover:border-[#1e1e1e]-400 transition"
       >
         <FaUpload className="mr-2" />
         Upload
