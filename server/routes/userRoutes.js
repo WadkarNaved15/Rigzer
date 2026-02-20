10e9e2d185661c7f1fdf5c63b08cb3fcd948293a
usaid <sayedusaid49@gmail.com>
2026-02-20 16:45:34 +0530

    Backend userRoutes changed

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // GET all users
 router.get("/", async (req, res) => {
   try {
-    const users = await User.find({}, "username email"); 
+    const users = await User.find({}, "username email avatar"); 
     // second arg selects only certain fields, you can add more if needed
     res.status(200).json(users);
   } catch (error) {
